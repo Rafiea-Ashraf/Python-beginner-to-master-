bd7c05b71023287ba77e4eca0f9f4e54d728e107
Rafiea-Ashraf <rafiea.ashraf@gmail.com>
2024-01-26 19:07:27 +0500

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+# we will learn about simple print commands and how error tells our mistakes
+print("Hello world")
+print("This wil be the first program we wil run in ur course")
+
+# string literal. THis tells us there is something wrong in our string. as we can see we are using two different
+# commas to close a string which is not right
+print('HEllo")
+      
+#this will give indentation error(spacing is not right)
+ print("Hello")
+ 
+# this will give brackets error which are incomplete in this case
+print("hello"
